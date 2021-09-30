99eee1e7835a1da29db69c92cd60964ce4782d97
Sourav Saha <souravsaha101@gmail.com>
2021-09-30 12:57:43 +0530

    project added

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c8e38251f724de27e5164d72cf506ac1'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '85b6cdd2-c0c1-4360-a665-14ea2a4977a1'
+};
